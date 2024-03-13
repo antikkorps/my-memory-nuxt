d032b5323d23128da0acda62a2151873c01d3e53
Franck <franckvienot7@gmail.com>
2024-03-13 17:18:53 +0100

    build the grid

--- components/MemoryGame.vue
+++ components/MemoryGame.vue
@@ -8,7 +8,7 @@ interface Card {
   matched: boolean
 }
 
-const image = [
+const images = [
   "https://images.unsplash.com/photo-1708355048964-1b52e054a3bd?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1708982553355-794739c6693e?q=80&w=2125&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1578305872374-2c938aea61ed?q=80&w=2154&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -22,9 +22,9 @@ const image = [
 const cards = ref<Card[]>([])
 
 const startGame = () => {
-  cards.value = image.map((img, index) => ({
+  cards.value = images.map((image, index) => ({
     id: index,
-    image: img,
+    image: image,
     flipped: false,
     matched: false,
   }))
@@ -32,6 +32,7 @@ const startGame = () => {
 
 const resetGame = () => {
   cards.value = []
+  startGame()
 }
 
 const flipCard = (card: Card) => {
@@ -55,15 +56,15 @@ onMounted(() => {
       <button @click="startGame">Start Game</button>
       <button @click="resetGame">Reset Game</button>
     </div>
-    <div class="grid grid-cols-3 gap-1 border-2 h-full">
+    <div class="grid grid-cols-4 grid-rows-4 sm:w-4/5 gap-2 mx-auto items-center">
       <div v-for="card in cards" :key="card.id" class="w-50 h-50">
-        <div @click="flipCard(card)" class="w-50 h-50 bg-red-400">
+        <div @click="flipCard(card)" class="w-52 h-52">
           <NuxtImg
             v-if="card.flipped || card.matched"
             :src="card.image"
             class="object-cover w-full h-full"
           />
-          <div v-else class="bg-gray-300 w-full h-full"></div>
+          <div v-else class="bg-gray-300 w-52 h-52"></div>
         </div>
       </div>
     </div>
