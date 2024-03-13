0ba07ff8e2534f9016a71fb08fee4b78ea42bba0
Franck <franckvienot7@gmail.com>
2024-03-13 17:43:51 +0100

    add timeout on opening the cards

--- components/MemoryGame.vue
+++ components/MemoryGame.vue
@@ -20,9 +20,13 @@ const images = [
 ]
 
 const cards = ref<Card[]>([])
+let selectedCards: Card[] = []
 
 const startGame = () => {
-  cards.value = images.map((image, index) => ({
+  const doubledImages = [...images, ...images]
+  const shuffledImages = doubledImages.sort(() => Math.random() - 0.5)
+
+  cards.value = shuffledImages.map((image, index) => ({
     id: index,
     image: image,
     flipped: false,
@@ -36,7 +40,24 @@ const resetGame = () => {
 }
 
 const flipCard = (card: Card) => {
-  card.flipped = !card.flipped
+  card.flipped = true
+  selectedCards.push(card)
+
+  if (selectedCards.length === 2) {
+    const [firstCard, secondCard] = selectedCards
+
+    if (firstCard.image !== secondCard.image) {
+      setTimeout(() => {
+        firstCard.flipped = false
+        secondCard.flipped = false
+        selectedCards = []
+      }, 1000)
+    } else {
+      firstCard.matched = true
+      secondCard.matched = true
+      selectedCards = []
+    }
+  }
 }
 
 onMounted(() => {
@@ -45,18 +66,12 @@ onMounted(() => {
 </script>
 <template>
   <div>
-    <h1>Memory Game</h1>
-    <NuxtImg
-      src="https://images.unsplash.com/photo-1708355048964-1b52e054a3bd?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      title="Memory Game"
-      width="200"
-      height="200"
-    />
-    <div>
-      <button @click="startGame">Start Game</button>
-      <button @click="resetGame">Reset Game</button>
-    </div>
-    <div class="grid grid-cols-4 grid-rows-4 sm:w-4/5 gap-2 mx-auto items-center">
+    <h1 class="text-center font-bold text-4xl my-4">Memory Game</h1>
+    <h2 class="text-center font-bold text-3xl my-4">Nombre de coups : xx</h2>
+
+    <div
+      class="grid md:grid-cols-2 lg:grid-cols-4 grid-rows-4 sm:w-4/5 gap-2 mx-auto items-center"
+    >
       <div v-for="card in cards" :key="card.id" class="w-50 h-50">
         <div @click="flipCard(card)" class="w-52 h-52">
           <NuxtImg
@@ -67,6 +82,11 @@ onMounted(() => {
           <div v-else class="bg-gray-300 w-52 h-52"></div>
         </div>
       </div>
+    </div>
+
+    <div>
+      <button @click="startGame">Start Game</button>
+      <button @click="resetGame">Reset Game</button>
     </div>
   </div>
 </template>
