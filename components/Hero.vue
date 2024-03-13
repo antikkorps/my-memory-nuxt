75004b52ba5a1d64027ea26c80fa49c591f5964b
Franck <franckvienot7@gmail.com>
2024-03-13 19:16:17 +0100

    added a Hero for the index with a link to the game

---
+++ components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Bienvenue sur MyMemory</h1>
+        <p class="py-6">Lancez une partie rapide</p>
+        <button class="btn btn-primary">
+          <NuxtLink to="/my-memory">Jouer !!!</NuxtLink>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1>Welcome to the memory game</h1>
+    <Hero />
   </div>
 </template>
